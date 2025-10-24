91a94f4cd35d803fa0b9dc5598397aa0dc21a213
icondenimStore <tn696199@gmail.com>
2025-10-24 16:57:08 +0700

    update

--- saleapp/index.py
+++ saleapp/index.py
@@ -1,13 +1,24 @@
 from pathlib import Path
 import sys
+
 ROOT_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT_DIR))
 
 from saleapp import app
 from flask import render_template
+import utils
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    cates = utils.load_categories()
+    return render_template('index.html', categories=cates)
+
+
+@app.route('/products')
+def product_list():
+    products = utils.load_products()
+    return render_template('products.html', products=products)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
