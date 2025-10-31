4337a70e998b4be1a724053254368563d3616606
icondenimStore <tn696199@gmail.com>
2025-10-31 17:38:05 +0700

    update

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@123@localhost/saleappdb?charset=utf8mb4'
+
+db = SQLAlchemy(app=app)
